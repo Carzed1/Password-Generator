3a15a5281ddf77c5d655407546a4f0fa05ba8941
Carzed1 <merisarkar22@gmail.com>
2023-10-05 21:35:52 +0530

    Password Generator Created

---
+++ index.js
@@ -0,0 +1,46 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+let passOne = document.getElementById("pass-one") 
+let passTwo = document.getElementById("pass-two") 
+
+let passwordOne = " "
+let passwordTwo = " "
+
+
+function createPassword(){
+    let random =" ";
+
+    for (i=0; i<15; i++) {
+        let index = Math.floor(Math.random() * characters.length)
+        random += characters[index]
+
+
+
+    }
+    return random
+}
+
+
+function generatePassword(){
+
+
+    passwordOne = createPassword()
+    passwordTwo = createPassword()
+
+
+
+    passOne.textContent = passwordOne;
+    passTwo.textContent = passwordTwo;
+    //  console.log(passwordOne)
+    // console.log(passwordTwo)
+    
+}
+
+passOne.addEventListener("click", function(){
+    const pass1 = passOne.textContent;
+    navigator.clipboard.writeText(pass1);
+})
+passTwo.addEventListener("click", function(){
+    const pass2 = passTwo.textContent;
+    navigator.clipboard.writeText(pass2);
+})
